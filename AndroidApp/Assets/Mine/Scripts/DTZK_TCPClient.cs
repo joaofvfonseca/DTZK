481e55361e50896f0c837ee6539ca9744472c726
João Fonseca <joaofvfonseca+github@gmail.com>
2019-07-16 02:34:51 +0100

    Take user input and status bar; Catching network errors

--- AndroidApp/Assets/Mine/Scripts/DTZK_TCPClient.cs
+++ AndroidApp/Assets/Mine/Scripts/DTZK_TCPClient.cs
@@ -1,26 +1,52 @@
 ﻿using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
+using TMPro;
 
 public class DTZK_TCPClient : MonoBehaviour
 {
     private string serverIP;
     private TcpClient socketConnection;
 
+    [SerializeField] private TextMeshProUGUI statusText;
+    [SerializeField] private TextMeshProUGUI inputText;
+
     void Start()
     {
-        SetServerIP("192.168.1.121");
+        statusText.text = "Disconnected";
     }
 
-    public void SetServerIP(string param)
+    public void SetServerIP()
     {
-        serverIP = param;
-        socketConnection = new TcpClient(serverIP, 12813);
+        serverIP = inputText.text;
+        Debug.Log(serverIP);
+        if (serverIP.Length < 8 || serverIP == null)
+        {
+            statusText.text = "Please type a valid IPv4 address";
+            return;
+        }
+        try
+        {
+            socketConnection = new TcpClient(serverIP, 12813);
+        }
+        catch (SocketException socketException)
+        {
+            statusText.text = socketException.ToString();
+            return;
+        }
+        if (socketConnection.GetStream().CanWrite)
+        {
+            statusText.text = "Connected @ " + serverIP;
+        }
+        else
+        {
+            statusText.text = "Could not connect, try again";
+        }
     }
 
     public void SendData(string param)
     {
-        if (serverIP == "")
+        if (serverIP == null)
         {
             return;
         }
